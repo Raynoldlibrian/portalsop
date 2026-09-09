c15e04591431c82c4638867df1ba029bbbf89289
Raynoldlibrian <97292115+Raynoldlibrian@users.noreply.github.com>
2026-09-09 14:32:58 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1745,7 +1745,7 @@ export default function SopPortal() {
         <div className="max-w-4xl mx-auto px-6 py-5 flex items-center justify-between gap-4">
           <div className="flex items-center gap-3">
             <img
-              src="https://i.imgur.com/jk45oWc.png"
+              src="https://i.imgur.com/gsA0FLG.png"
               alt="Logo Kabupaten Indragiri Hulu"
               className="shrink-0 w-12 h-12 object-contain"
             />
